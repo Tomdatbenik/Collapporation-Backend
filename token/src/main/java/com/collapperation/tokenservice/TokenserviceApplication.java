964a0f220e6c0f1c85f88fb9fea69965c2b076b2
Niek125 <niek.sleddens@gmail.com>
2020-03-25 11:46:46 +0100

    re added tokenservice

--- token/src/main/java/com/collapperation/tokenservice/TokenserviceApplication.java
+++ token/src/main/java/com/collapperation/tokenservice/TokenserviceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TokenServiceApplication {
+public class TokenserviceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TokenServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(TokenserviceApplication.class, args);
+	}
 
 }
