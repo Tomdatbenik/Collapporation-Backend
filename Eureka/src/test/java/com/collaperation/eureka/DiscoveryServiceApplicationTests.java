7896fc21b9db8dbd7bfc6374ffc410e652efeca0
Niek125 <niek.sleddens@gmail.com>
2020-04-01 10:40:24 +0200

    Discovery service added +

---
+++ Eureka/src/test/java/com/collaperation/eureka/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collaperation.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
