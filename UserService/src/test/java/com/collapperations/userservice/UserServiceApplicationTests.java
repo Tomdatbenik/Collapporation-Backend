ea195130b39d681ef64d6e10ec5a78c7a5a400dc
Niek125 <niek.sleddens@gmail.com>
2020-04-08 14:04:06 +0200

    user service init

---
+++ UserService/src/test/java/com/collapperations/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collapperations.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
