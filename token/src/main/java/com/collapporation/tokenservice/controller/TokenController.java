8283a27b92887ab27addca684d46025ecd557890
Niek125 <niek.sleddens@gmail.com>
2020-04-15 09:28:32 +0200

    added cors

--- token/src/main/java/com/collapporation/tokenservice/controller/TokenController.java
+++ token/src/main/java/com/collapporation/tokenservice/controller/TokenController.java
@@ -13,11 +13,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @AllArgsConstructor
 @RequestMapping("/token")
