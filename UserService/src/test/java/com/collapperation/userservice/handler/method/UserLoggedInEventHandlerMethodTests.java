3762835e2a69fafa939ec4c57b86b65a13bb99fe
Niek125 <niek.sleddens@gmail.com>
2020-04-21 11:37:56 +0200

    added test classs

---
+++ UserService/src/test/java/com/collapperation/userservice/handler/method/UserLoggedInEventHandlerMethodTests.java
@@ -0,0 +1,9 @@
+package com.collapperation.userservice.handler.method;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@DirtiesContext
+@SpringBootTest
+public class UserLoggedInEventHandlerMethodTests {
+}
