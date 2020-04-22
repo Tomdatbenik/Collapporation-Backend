36746c26b31182e33917cfb8cca2d9d776f080c7
Niek125 <niek.sleddens@gmail.com>
2020-04-22 14:03:42 +0200

    event handler tests

--- UserService/src/test/java/com/collapperation/userservice/handler/EventHandlerTests.java
+++ UserService/src/test/java/com/collapperation/userservice/handler/EventHandlerTests.java
@@ -1,7 +1,52 @@
 package com.collapperation.userservice.handler;
 
+import com.collapperation.userservice.event.Event;
+import com.collapperation.userservice.event.UserLoggedInEvent;
+import com.collapperation.userservice.model.User;
+import com.collapperation.userservice.repo.UserRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
+
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
+@Sql(scripts = {"/test/user.sql"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class EventHandlerTests {
+    @Autowired
+    private EventHandler eventHandler;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Test
+    @Transactional
+    public void processEventTest(){
+        final String id = "0";
+        final String username = "new userName";
+        final String picture = "new picture";
+
+        eventHandler.processEvent(new UserLoggedInEvent(id, username, picture));
+        final User user = userRepo.getOne(id);
+
+        assertThat(user).isNotNull();
+        assertThat(user.getId()).isEqualTo(id);
+        assertThat(user.getUsername()).isEqualTo(username);
+        assertThat(user.getPicture()).isEqualTo(picture);
+    }
+
+    private class UnknownEvent extends Event {
+    }
+
+    @Test
+    public void processUnknownEventTest(){
+        assertDoesNotThrow(() -> {
+            eventHandler.processEvent(new UnknownEvent());});
+    }
 }
