db2cbe43574247842d6c98e4e880d9842d1c278f
Niek125 <niek.sleddens@gmail.com>
2020-04-22 14:46:24 +0200

    model defintion

--- UserService/src/main/java/com/collapperation/userservice/model/User.java
+++ UserService/src/main/java/com/collapperation/userservice/model/User.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
@@ -26,8 +27,15 @@ public class User {
     private String firstName;
     @Length(min = 0, max = 256)
     private String lastName;
-    @Length(min = 0, max = 2147483647)
+    @Column(columnDefinition = "Text")
     private String bio;
     @URL
     private String picture;
+    private String functionTitle;
+    private String email;
 }
+//tags
+//links
+//projects
+
+//user -> user name, image, id, role
