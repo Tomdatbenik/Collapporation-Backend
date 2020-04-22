4c41647cf7223673419131afb695e83dbbfed752
Tomdatbenik <tomvankaathoven@gmail.com>
2020-04-22 14:50:21 +0200

    Update ProjectRepo.java

--- ProjectService/src/main/java/com/collapporation/projectservice/repo/ProjectRepo.java
+++ ProjectService/src/main/java/com/collapporation/projectservice/repo/ProjectRepo.java
@@ -9,12 +9,11 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface ProjectRepo extends JpaRepository<Project, String> {
-    
+
     List<ProjectFeedDTO> findAllByOrderByCreatedDesc(Pageable pageable);
 
     @Modifying
