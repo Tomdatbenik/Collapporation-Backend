c8e09892a4e93d8c6b1f4404aaae7e51d4bf5bf4
Niek125 <niek.sleddens@gmail.com>
2020-04-22 12:13:39 +0200

    full transaction tested

--- UserService/src/test/java/com/collapperation/userservice/handler/method/UserLoggedInEventHandlerMethodTests.java
+++ UserService/src/test/java/com/collapperation/userservice/handler/method/UserLoggedInEventHandlerMethodTests.java
@@ -1,5 +1,7 @@
 package com.collapperation.userservice.handler.method;
 
+import com.collapperation.userservice.event.UserLoggedInEvent;
+import com.collapperation.userservice.model.User;
 import com.collapperation.userservice.repo.UserRepo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -7,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
+
+import javax.transaction.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -28,11 +32,23 @@ public class UserLoggedInEventHandlerMethodTests {
         final UserLoggedInEventHandlerMethod userLoggedInEventHandlerMethod = new UserLoggedInEventHandlerMethod(userRepo);
 
         assertThat(userLoggedInEventHandlerMethod).isNotNull();
-        assertThat(userLoggedInEventHandlerMethod.getHandlingType()).isOfAnyClassIn(UserLoggedInEventHandlerMethod.class);
+        assertThat(userLoggedInEventHandlerMethod.getHandlingType()).isEqualTo(UserLoggedInEvent.class);
     }
 
     @Test
+    @Transactional
     public void updatesUserBasicInfoTest(){
+        final String id = "0";
+        final String username = "new userName";
+        final String picture = "new picture";
+        final UserLoggedInEventHandlerMethod userLoggedInEventHandlerMethod = new UserLoggedInEventHandlerMethod(userRepo);
 
+        userLoggedInEventHandlerMethod.handle(new UserLoggedInEvent(id, username, picture));
+        final User user = userRepo.getOne(id);
+
+        assertThat(user).isNotNull();
+        assertThat(user.getId()).isEqualTo(id);
+        assertThat(user.getUsername()).isEqualTo(username);
+        assertThat(user.getPicture()).isEqualTo(picture);
     }
 }
