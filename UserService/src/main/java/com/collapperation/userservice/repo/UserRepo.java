3bfa2048175ed24783079e4b99f97e5600b58b2d
Niek125 <niek.sleddens@gmail.com>
2020-05-06 13:32:52 +0200

    kafka serializer tests

--- UserService/src/main/java/com/collapperation/userservice/repo/UserRepo.java
+++ UserService/src/main/java/com/collapperation/userservice/repo/UserRepo.java
@@ -9,5 +9,5 @@ import org.springframework.data.repository.query.Param;
 public interface UserRepo extends JpaRepository<User, String> {
     @Modifying
     @Query("UPDATE user u SET u.picture = :newPfp, u.username = :newUserName WHERE u.id = :id")
-    public void updateBasicUserInfo(@Param("newPfp") String pfp, @Param("newUserName") String userName, @Param("id") String id);
+    void updateBasicUserInfo(@Param("newPfp") String pfp, @Param("newUserName") String userName, @Param("id") String id);
 }
