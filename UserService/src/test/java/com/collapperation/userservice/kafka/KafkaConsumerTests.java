e3ab0fbb55443e11afaf72ecf1788e8eb4fd697c
Niek125 <niek.sleddens@gmail.com>
2020-05-06 15:37:07 +0200

    embedded kafka tests --needs compatible versions

--- UserService/src/test/java/com/collapperation/userservice/kafka/KafkaConsumerTests.java
+++ UserService/src/test/java/com/collapperation/userservice/kafka/KafkaConsumerTests.java
@@ -1,8 +1,62 @@
 package com.collapperation.userservice.kafka;
 
+import com.collapperation.userservice.model.User;
+import com.collapperation.userservice.repo.UserRepo;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.rule.KafkaEmbedded;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
-@EmbeddedKafka
+@EmbeddedKafka(topics = "user", partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 public class KafkaConsumerTests {
+    @Autowired
+    private KafkaConsumer kafkaConsumer;
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    public KafkaTemplate<String, String> template;
+
+    @Autowired
+    public KafkaEmbedded kafkaEmbedded;
+
+    @ClassRule
+    public static KafkaEmbedded embeddedKafka = new KafkaEmbedded(1, true, 0);
+
+    @BeforeClass
+    public static void setUpClass() {
+        System.setProperty("spring.kafka.bootstrap-servers", embeddedKafka.getBrokersAsString());
+        System.setProperty("spring.cloud.stream.kafka.binder.zkNodes", embeddedKafka.getZookeeperConnectionString());
+    }
+
+    @Test
+    public void constructorTest() {
+        assertDoesNotThrow(() -> {
+            new KafkaConsumer(null);
+        });
+
+        KafkaConsumer kafkaConsumer = new KafkaConsumer(null);
+        assertThat(kafkaConsumer).isNotNull();
+    }
+
+    @Test
+    public void consumeTest() {
+
+
+        template.send("user", "UserLoggedInEvent.{\"uuid\":\"id\",\"username\":\"username\",\"picture\":\"picture\"}");
+        final User user = userRepo.getOne("id");
+
+        assertThat(user).isNotNull();
+        assertThat(user.getPicture()).isEqualTo("picture");
+        assertThat(user.getUsername()).isEqualTo("username");
+        assertThat(user.getId()).isEqualTo("id");
+    }
 }
