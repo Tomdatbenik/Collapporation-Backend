3982cf43877dc8202e5579ca87239b675894b7ab
Niek125 <niek.sleddens@gmail.com>
2020-05-12 13:18:12 +0200

    removed cross origin

--- token/src/main/java/com/collapporation/tokenservice/controller/TokenController.java
+++ token/src/main/java/com/collapporation/tokenservice/controller/TokenController.java
@@ -13,9 +13,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
 @AllArgsConstructor
 @RequestMapping("/token")
