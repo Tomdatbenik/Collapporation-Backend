8e27be9d85f8db7cfc306ca218248681cb5d4db3
Tomdatbenik <tomvankaathoven@gmail.com>
2020-06-02 13:56:05 +0200

    Update ValidatedLikeEventHandlerMethod.java

--- LikeService/src/main/java/com/collapporation/likeservice/handler/method/ValidatedLikeEventHandlerMethod.java
+++ LikeService/src/main/java/com/collapporation/likeservice/handler/method/ValidatedLikeEventHandlerMethod.java
@@ -1,24 +1,25 @@
 package com.collapporation.likeservice.handler.method;
 
 import com.collapporation.likeservice.event.LikeValidatedEvent;
+import com.collapporation.likeservice.event.ValidateLikeEvent;
 import com.collapporation.likeservice.repo.LikeRepo;
 import com.collapporation.likeservice.handler.HandlerMethod;
 import com.collapporation.likeservice.models.Like;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ValidatedLikeEventHandlerMethod extends HandlerMethod<LikeValidatedEvent>
+public class ValidatedLikeEventHandlerMethod extends HandlerMethod<ValidateLikeEvent>
 {
     private final LikeRepo likeRepo;
 
     protected ValidatedLikeEventHandlerMethod(LikeRepo likeRepo)
     {
-        super(LikeValidatedEvent.class);
+        super(ValidateLikeEvent.class);
         this.likeRepo = likeRepo;
     }
 
     @Override
-    public void handle(LikeValidatedEvent event)
+    public void handle(ValidateLikeEvent event)
     {
         likeRepo.save(new Like(event.getObject_id(),event.getLiked_by_id()));
     }
