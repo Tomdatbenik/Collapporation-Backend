b325bf999451b5ca774b9c24c3035269413a63dc
Tomdatbenik <tomvankaathoven@gmail.com>
2020-06-16 09:59:57 +0200

    Refreshed token

--- LikeService/src/test/java/com/collapporation/likeservice/controller/LikeControllerTests.java
+++ LikeService/src/test/java/com/collapporation/likeservice/controller/LikeControllerTests.java
@@ -37,7 +37,7 @@ public class LikeControllerTests {
     void likeSuccessfulTest() throws Exception {
         mockMvc.perform(
                 MockMvcRequestBuilders.post("/like/like").contentType(MediaType.APPLICATION_JSON)
-                        .header("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9.eyJ1c2VySW1hZ2UiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS0vQU9oMTRHaEpzN1pqaXpVWGtWbXNZRFZlVmdyQ00xdjlvRHZIclNlOTJIYklKdyIsImlzcyI6ImNvbGxhcHBlcmF0aW9uLXRva2VuLXNlcnZpY2UiLCJleHAiOjE1OTIwMzcyNTgsInVzZXJOYW1lIjoiV29uZGVyZnVsUHVnaWxpc3QiLCJpYXQiOjE1OTA1NjYwMjksInV1aWQiOiJmZlU2Z1pudU9maGpidjFlV09OMmJoVnV6azgzIiwianRpIjoiOWJlNWU5OTEtNzIzNi00Mjk2LWFhNjctN2FkMDM5NDczMjRkIn0.TYpspJtOQlgntNpxDnEHUGBUj8oyABBo4HfB8OElNDtf428KQTHj8uhPoMxKYaJDkFpo4FeZN3Wl66GiPhiYYVnEcupatliRRYadK3IklFD30FbJW9NLgRaFu3buZBpqXAu_tUIBC0Egb8jQT6ETipDr54_r3cQirmsNtU5RUls")
+                        .header("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9.eyJ1c2VySW1hZ2UiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS0vQU9oMTRHamtFbjVHbC1iZlZhRGxuZ204eHhFOGFfVVJtUkRzNUJkeENaYTlldyIsImlzcyI6ImNvbGxhcHBlcmF0aW9uLXRva2VuLXNlcnZpY2UiLCJleHAiOjE1OTM3NjUxMTUsInVzZXJOYW1lIjoiUm95IEFwcGVsZG9vcm4iLCJpYXQiOjE1OTIyOTM4ODYsInV1aWQiOiJLU2FoRURORjZJVUhicVkwNDE0eGIxbGs0MjczIiwianRpIjoiZTI1MmUyNTItNTQxZS00OWI0LTk5ZDUtNTQzODhjNWE2Y2VlIn0.KZoUob_GLpoeaqVFwDHMXc0LyULTaIfzAPAAKhe7T5jlWyn91ltBEPJpTWDSIEctGmJelwF5BjpU-Qjvp0R_l18aT7ovbYjeZAsA3tC5CIsYSl4MtOklbOevvr8dM2W6sEk9ucxwHBcQP0fzdMaRAXATiHtJIk73E7ICXo_cuMk")
                         .content("{\"object_id\":\"1\",\"liked_by_id\":\"2\"}")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
