2609f39299e58c4a3e4dbdd8d50626ccbd752b26
BeauTaapken <beautaapken@me.com>
2020-06-16 11:02:24 +0200

    * Added logging tom should have added

--- ProjectService/src/main/java/com/collapporation/projectservice/service/FeedService.java
+++ ProjectService/src/main/java/com/collapporation/projectservice/service/FeedService.java
@@ -4,6 +4,7 @@ import com.collapporation.projectservice.models.Project;
 import com.collapporation.projectservice.models.dto.ProjectFeedDTO;
 import com.collapporation.projectservice.repo.ProjectRepo;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,30 +14,38 @@ import java.util.*;
 
 @Service
 @AllArgsConstructor
+@Log4j2
 public class FeedService {
     private final ProjectRepo projectRepo;
 
     private final RestTemplate restTemplate;
 
     public List<ProjectFeedDTO> getProjectFeed(int page, int size){
+        log.info("new request");
         Pageable pageable = PageRequest.of(page, size);
+
+        log.info("getting page");
         List<Project> projectList = projectRepo.findAllByOrderByCreatedDesc(pageable);
 
         List<ProjectFeedDTO> projectFeedList = new ArrayList<>();
 
         projectList.stream().forEach(p -> {
+            log.info("getting owner: {}", p.getOwnerId());
             ProjectFeedDTO projectFeedDTO = new ProjectFeedDTO(p);
-            try{
-                //TODO get user or group instead of always trying to get a user
-                projectFeedDTO.setOwner(restTemplate.getForObject("http://user-service/user/" + p.getOwnerId(), String.class));
-            }
-            catch (Exception ex) {
-                projectFeedDTO.setOwner("{ name: 'no user could be found' }");
-            }
+            projectFeedDTO.setOwner(restTemplate.getForObject("http://user-service/user/" + p.getOwnerId(), String.class));
+//            try{
+//                //TODO get user or group instead of always trying to get a user
+//                projectFeedDTO.setOwner(restTemplate.getForObject("http://user-service/user/" + p.getOwnerId(), String.class));
+//            }
+//            catch (Exception ex) {
+//                projectFeedDTO.setOwner("{ name: 'no user could be found' }");
+//            }
+            log.info("adding project");
             projectFeedList.add(projectFeedDTO);
         });
 
         projectFeedList.removeIf(Objects::isNull);
+        log.info("returning");
         return projectFeedList;
     }
 }
